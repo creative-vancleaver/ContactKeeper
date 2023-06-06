f022c216580710f2616542aa7bdb2eaa400c3a23
JVC <vancleave.jacob@gmail.com>
2023-06-06 16:19:06 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,12 +10,22 @@ connectDB();
 // INIT MIDDLEWARE
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeper API!' }));
+// app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeper API!' }));
 
 // DEFINE ROUTES
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/auth/', require('./routes/auth'));
 app.use('/api/contacts/', require('./routes/contacts'));
+
+// SERVE STATIC ASSETS
+if (process.env.NODE_ENV === 'production') {
+
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'));
+
+  // CREATE ROUTE (ANYTHING NOT DEFINED ABOVE)
+  app.get('*', (req, res) => res.sendFild(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5555;
 
